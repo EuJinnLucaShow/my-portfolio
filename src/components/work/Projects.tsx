b0826208bc8ead7ba164a606e8caeb931a216da5
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2025-03-13 20:42:30 +0200

    Update Projects.tsx

--- src/components/work/Projects.tsx
+++ src/components/work/Projects.tsx
@@ -3,15 +3,17 @@ import { Column } from "@/once-ui/components";
 import { ProjectCard } from "@/components";
 
 interface ProjectsProps {
-  range?: [number, number?];
+  readonly range?: readonly [number, number?];
 }
 
 export function Projects({ range }: ProjectsProps) {
   let allProjects = getPosts(["src", "app", "work", "projects"]);
 
-  const sortedProjects = allProjects.sort((a, b) => {
-    return new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime();
-  });
+  const sortedProjects = allProjects.toSorted(
+    (a, b) =>
+      new Date(b.metadata.publishedAt).getTime() -
+      new Date(a.metadata.publishedAt).getTime()
+  );
 
   const displayedProjects = range
     ? sortedProjects.slice(range[0] - 1, range[1] ?? sortedProjects.length)
@@ -28,7 +30,9 @@ export function Projects({ range }: ProjectsProps) {
           title={post.metadata.title}
           description={post.metadata.summary}
           content={post.content}
-          avatars={post.metadata.team?.map((member) => ({ src: member.avatar })) || []}
+          avatars={
+            post.metadata.team?.map((member) => ({ src: member.avatar })) || []
+          }
           link={post.metadata.link ?? ""}
         />
       ))}
